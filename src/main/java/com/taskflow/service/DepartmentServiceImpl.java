5f49689cbd5971c68583dcde1cd09fba473e3473
suniljakkula <sunij8911@gmail.com>
2026-01-17 13:11:33 +0530

    added the department service and controller classes

--- src/main/java/com/taskflow/service/DepartmentServiceImpl.java
+++ src/main/java/com/taskflow/service/DepartmentServiceImpl.java
@@ -1,5 +1,58 @@
 package com.taskflow.service;
 
-public class DepartmentServiceImpl {
+import java.util.List;
+import java.util.Objects;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.taskflow.dao.DepartmentRepository;
+import com.taskflow.model.Department;
+
+@Service
+public class DepartmentServiceImpl implements DepartmentService {
+
+	@Autowired
+	private DepartmentRepository departmentRepository;
+
+	@Override
+	public Department saveDepartment(Department department) {
+		// TODO Auto-generated method stub
+		return departmentRepository.save(department);
+	}
+
+	@Override
+	public List<Department> fetchDepartmentList() {
+		// TODO Auto-generated method stub
+		return (List<Department>) departmentRepository.findAll();
+	}
+
+	@Override
+	public Department updateDepartment(Department department, Long departmentId) {
+		// TODO Auto-generated method stub
+
+		Department depDB = departmentRepository.findById(departmentId).get();
+
+		if (Objects.nonNull(department.getDepartmentName()) && !"".equalsIgnoreCase(department.getDepartmentName())) {
+			depDB.setDepartmentName(department.getDepartmentName());
+		}
+
+		if (Objects.nonNull(department.getDepartmentAddress())
+				&& !"".equalsIgnoreCase(department.getDepartmentAddress())) {
+			depDB.setDepartmentAddress(department.getDepartmentAddress());
+		}
+
+		if (Objects.nonNull(department.getDepartmentCode()) && !"".equalsIgnoreCase(department.getDepartmentCode())) {
+			depDB.setDepartmentCode(department.getDepartmentCode());
+		}
+
+		return departmentRepository.save(depDB);
+	}
+
+	@Override
+	public void deleteDepartmentById(Long departmentId) {
+		// TODO Auto-generated method stub
+		departmentRepository.deleteById(departmentId);
+	}
 
 }
