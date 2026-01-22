6e5238cb89dcfcfaf34f0ba506e495a79f8061dd
suniljakkula <sunij8911@gmail.com>
2026-01-22 06:17:15 +0530

    pom.xml issues got resolved

--- src/main/java/com/taskflow/model/Department.java
+++ src/main/java/com/taskflow/model/Department.java
@@ -3,11 +3,15 @@ package com.taskflow.model;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name="DEPARTMENT")
 public class Department {
 
 	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Id
 	private Long departmentId;
 	private String departmentName;
 	private String departmentAddress;
--- src/test/java/com/taskflow/TaskflowApplicationTests.java
+++ src/test/java/com/taskflow/TaskflowApplicationTests.java
@@ -1,4 +1,4 @@
-package java.com.taskflow;
+package com.taskflow;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
