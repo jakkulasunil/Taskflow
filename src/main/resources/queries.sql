2c848f10b7e5796d97d7011b904f2bec89d8fb07
suniljakkula <sunij8911@gmail.com>
2026-01-25 22:49:01 +0530

    added one more record

--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -1,7 +1,9 @@
 select * from department;
-INSERT INTO department (department_address, department_code, department_name)
+INSERT INTO department (department_id,department_address, department_code, department_name)
 VALUES (1, 'hyderabad', '1001', 'IT');
 INSERT INTO department (department_id,department_address, department_code, department_name)
 VALUES (2,'hyderabad', '1002', 'CSE');
 INSERT INTO department (department_id,department_address, department_code, department_name)
 VALUES (3,'hyderabad', '1003', 'EEE');
+INSERT INTO department (department_id,department_address, department_code, department_name)
+VALUES (4,'hyderabad', '1004', 'ECE');
