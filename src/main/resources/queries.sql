305876544adafd54c6f63a5e54203e4e0e4b2c8d
suniljakkula <sunij8911@gmail.com>
2026-01-26 21:26:52 +0530

    query added

--- src/main/resources/queries.sql
+++ src/main/resources/queries.sql
@@ -7,3 +7,5 @@ INSERT INTO department (department_id,department_address, department_code, depar
 VALUES (3,'hyderabad', '1003', 'EEE');
 INSERT INTO department (department_id,department_address, department_code, department_name)
 VALUES (4,'hyderabad', '1004', 'ECE');
+INSERT INTO department (department_id,department_address, department_code, department_name)
+VALUES (5,'hyderabad', '1005', 'CIVIL');
