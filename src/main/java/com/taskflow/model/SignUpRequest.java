48cb94ea8aa92c7ca2d1696fa5d9bbf2ca59c56b
suniljakkula <sunij8911@gmail.com>
2026-01-31 22:59:22 +0530

    constructor created

--- src/main/java/com/taskflow/model/SignUpRequest.java
+++ src/main/java/com/taskflow/model/SignUpRequest.java
@@ -2,37 +2,50 @@ package com.taskflow.model;
 
 public class SignUpRequest {
 	private String name;
-    private String email;
-    private String mobile;
-    private String password;
+	private String email;
+	private String mobile;
+	private String password;
+
+	public SignUpRequest() {
+		// TODO Auto-generated constructor stub
+	}
+
 	public String getName() {
 		return name;
 	}
+
 	public void setName(String name) {
 		this.name = name;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
+
 	public String getMobile() {
 		return mobile;
 	}
+
 	public void setMobile(String mobile) {
 		this.mobile = mobile;
 	}
+
 	public String getPassword() {
 		return password;
 	}
+
 	public void setPassword(String password) {
 		this.password = password;
 	}
+
 	@Override
 	public String toString() {
 		return "SignUpRequest [name=" + name + ", email=" + email + ", mobile=" + mobile + ", password=" + password
 				+ "]";
 	}
-        
+
 }
